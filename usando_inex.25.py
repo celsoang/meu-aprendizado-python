e9e5378d5acb189801067c1330361a9bb732ac46
celsoang <celsozxc@gmail.com>
2026-07-15 00:45:45 -0300

    Adiciona exercicio usando_inex.25.py

---
+++ usando_inex.25.py
@@ -0,0 +1,7 @@
+print('Verificação se nome possui silva. ')
+nome = input('Digite seu nome: ')
+print('silva' in nome)
+if 'silva' in nome.lower():
+    print('Seu nome possui silva!')
+else:
+    print('Seu nome não possui silva!')
